b187930e0cf4a9f4a8cac2225b70859af59258f1
AlPHA__830 <68626539+CoachAlpha@users.noreply.github.com>
2022-06-12 23:48:37 -0400

    time edit

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ status = ["Check out am1r.tech", "ZEDSIO Studio is officially LIVE", "JUST A BOR
 token = os.environ.get('token')
 emojies = ["📢", "🎮", "👨‍💻", "💻", "💬"]
 emo = ""
-delay = 2
+delay = 20
 class main:
     def __init__(self, token, status):
         self.token = token
