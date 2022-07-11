7a685184feaecce55877ba87a5b90a9dd199deee
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2022-07-11 15:43:00 -0700

    added new bios

--- script.py
+++ script.py
@@ -4,9 +4,9 @@ import time
 import os
 import random
 
-status = ["Website: am1r.tech", "IG: @am1r__8", "JUST A BORED PROGRAMMER", "I call my self a developer but I can't develop a fucking relationship", "I didn't run this time did I?"]
+status = ["Website: am1r.tech", "IG: @am1r__8", "JUST A BORED PROGRAMMER", "I call my self a developer but I can't develop a fucking relationship", "are you a rusher, or are you a dragger or are you gonna be ON MY FUCKING TIME?", "I gotta tell u it was perfect, perfect everything down to the last minute details"]
 token = os.environ.get('token')
-emojies = ["📢", "🎮", "👨‍💻", "💻", "💬"]
+emojies = ["📢", "🎮", "👨‍💻", "💻", "💬", "✨"]
 photos = []
 emo = ""
 delay = 30
